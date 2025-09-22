7b911786242c382a32adfacbbc65463dc1a165bb
Deneziz <b00177540@mytudublin.ie>
2025-09-22 16:04:27 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello, World 
+      const [fullname, setFullname] = useState("Eoin OKennedy");
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
