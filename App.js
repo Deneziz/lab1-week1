15d9fe6a23d7a2d76171fc5bd477fb1b8a0564ee
Deneziz <b00177540@mytudublin.ie>
2025-09-22 16:15:58 +0100

    Update App.js.Part 2

--- App.js
+++ App.js
@@ -1,15 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React, {useState} from 'react';
-const styles = StyleSheet.create({
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
-
-
 export default function App() {
   return (
     <View>
@@ -19,4 +9,12 @@ export default function App() {
     </View>
   );
 }
-
+const styles = StyleSheet.create({
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    textDecorationLine: 'underline',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
