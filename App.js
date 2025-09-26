eb044d6513d8a5e109b9d8f4c9bb64b6c8d6e2c9
Deneziz <b00177540@mytudublin.ie>
2025-09-26 22:10:35 +0100

    Update_full_App.js
    fineshed step 4

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 export default function App() {
+
+  const [fullname, setFullname] = useState("Denys Bondarenko");
+
   return (
     <View>
-      <Text style={styles.paragraph}>Hello, World 
-      const [fullname, setFullname] = useState("Eoin OKennedy");
-      </Text>
+      <Text style={styles.paragraph}>Hello, World, {fullname}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your name"
+        onChangeText={text => setFullname(text)} 
+      />
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   paragraph: {
     margin: 24,
@@ -16,5 +23,11 @@ const styles = StyleSheet.create({
     textDecorationLine: 'underline',
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
